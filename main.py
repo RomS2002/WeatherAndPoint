5d82742dc11d42c96950120cf49fca4b8325ba11
RomS2002 <romsyt2002@gmail.com>
2025-09-24 20:20:12 +0300

    Предварительная версия

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI, Depends, Response, Request, Form, status
-from fastapi.responses import RedirectResponse, FileResponse, PlainTextResponse, HTMLResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
-from user_service import UserService
+from user_service import UserService, User
 from session_layer import validate_session, create_session
-
-user = None
 
 us = UserService()
 app = FastAPI()
@@ -23,24 +21,34 @@ async def root(request: Request, is_valid_session = Depends(validate_session)):
         return RedirectResponse("/logout", status_code=status.HTTP_303_SEE_OTHER)
 
     username = request.session.get("username")
+
     user = us.find_user_by_username(username)
     weather = user.weather
     context = {"username":username, "body_class":weather}
     return templates.TemplateResponse(request=request, name="index.html", context=context)
 
 @app.post('/')
-async def changeWeather(weather: Annotated[str, Form()]):
+async def change_weather(request: Request, weather: Annotated[str, Form()]):
+    username = request.session.get("username")
+    user = us.find_user_by_username(username)
     if weather:
-        us.update_user_weather()
+        us.update_user_weather(user.usr_id, weather)
+        return RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
     return RedirectResponse("/logout", status_code=status.HTTP_303_SEE_OTHER)
 
 @app.get('/login')
-async def show_login_page():
-    return FileResponse("public/login.html")
+async def show_login_page(request: Request):
+    return templates.TemplateResponse(request=request, name="login.html")
 
 @app.post('/login', response_class=RedirectResponse)
 async def do_login(username: Annotated[str, Form()], password: Annotated[str, Form()],
                    request: Request):
+    found_user = us.find_user_by_username(username)
+    if not found_user:
+        return templates.TemplateResponse(request=request, name="login.html", context={"username_error": "yes"})
+    elif found_user.password != password:
+        return templates.TemplateResponse(request=request, name="login.html", context={"password_error": "yes"})
+
     response = RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
     create_session((request, response))
     request.session["username"] = username
@@ -48,13 +56,30 @@ async def do_login(username: Annotated[str, Form()], password: Annotated[str, Fo
     return response
 
 @app.get('/register')
-async def show_register_page():
-    return FileResponse("public/register.html")
+async def show_register_page(request: Request):
+    return templates.TemplateResponse(request=request, name="register.html")
+
+@app.post('/register')
+async def do_register(username: Annotated[str, Form()], password: Annotated[str, Form()],
+                      password2: Annotated[str, Form()], request: Request):
+    found_user = us.find_user_by_username(username)
+    if found_user:
+        return templates.TemplateResponse(request=request, name="register.html", context={"username_error":"yes"})
+    if password != password2:
+        return templates.TemplateResponse(request=request, name="register.html", context={"password_error":"yes"})
+
+    new_user = User(username=username, password=password, weather="sunny")
+    new_user.usr_id = us.add_new_user(new_user)
+
+    response = RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
+    create_session((request, response))
+    request.session["username"] = username
+    print(f"Response headers: {response.raw_headers}")
+    return response
+
 
 @app.get("/logout")
 async def logout(request: Request, response: RedirectResponse):
     request.session.clear()
     response.delete_cookie(key="Authorization")
-    global user
-    user = None
     return RedirectResponse("/login", status_code=status.HTTP_303_SEE_OTHER)
